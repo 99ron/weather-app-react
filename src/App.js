f5b896ff199cfa76959443873ad2fac062e12562
Aaron J. Miles <aaron.miles@rmays.com>
2021-09-28 12:39:35 +0100

    Initial Commit. Basic styling added for app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: "021289b2046100cd59b0bac35e3c91cb",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+        <div className="location-box">
+          <div className="location">Fareham, UK</div>
+          <div className="date"></div>
+
+        </div>
+      </main>
     </div>
   );
 }
